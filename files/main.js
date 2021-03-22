5a6beeae262f9abd297fafc16c6b8bc6ba7e40c4
Eymen Batın Yacı <39139996+eymenyaci@users.noreply.github.com>
2021-03-22 23:46:13 +0300

    last commit

--- files/main.js
+++ files/main.js
@@ -1,4 +1,4 @@
-﻿let chartWrap = document.getElementById('chart-wrap');
+﻿let chartWrap = document.getElementById('chart-wrap'); // chart load area
 
 let countries = document.getElementById('countries');
 let status = document.getElementById('status');
@@ -6,19 +6,28 @@ let status = document.getElementById('status');
 let selectedCountry = null;
 let selectedStatus = null;
 
+// ilk çalışan metod - uygulama açılır açılmaz
 const init = function () {
+    renderNav();
+
+    // dönen ülkeleri countries elementine option elementi olarak ekler
     getCountries().then(res => {
+        res.sort((a, b) => (a['Slug'] > b['Slug']) ? 1 : -1); // alfabeye göre sıralama
+        // tek tek tüm ülkeleri dönüyoruz ve option elementi haline getirip countries elementine ekliyoruz.
         res.forEach(country => {
             let option = document.createElement('option');
             option.value = country['Slug'];
             option.text = country['Country'];
-
-            countries.appendChild(option);
+            if (option.value == "turkey") {
+                option.selected = true;
+            }
+            countries.appendChild(option); // option elementi ekliyor
         });
+        send(); // auto send
     });
-    renderNav();
 }
 
+// status değişkeninin olduğu elemente değerleri yükler
 const renderNav = function () {
     let items = [
         "Confirmed",
@@ -34,18 +43,24 @@ const renderNav = function () {
         let option = document.createElement('option');
         option.value = s;
         option.text = s;
-        status.appendChild(option);
+        if (s == "Confirmed-Daily") {
+            option.selected = true;
+        }
+        status.appendChild(option); // status elementine option elementi ekler
     });
 }
 
+// ülkeler verilerini getiriyor
 const getCountries = async function () {
     return request('https://api.covid19api.com/countries');
 }
 
+// ülke slug ına göre verileri getiriyor
 const getDayOneData = async function (slug) {
     return request('https://api.covid19api.com/total/dayone/country/' + slug);
 }
 
+// api istek gönderme metodu
 const request = async function(url, type = "GET") {
     return await fetch(url, {
         method: type,
@@ -57,148 +72,82 @@ const request = async function(url, type = "GET") {
 }
 
 
+// göster butonuna basınca çalışır
 const send = function () {
     selectedStatus = status.value;
     selectedCountry = countries.value;
 
-    if (selectedStatus == null || selectedStatus == "") {
-        alert('Lütfen Durum Seçimi Yapınız');
-    }
-    else if (selectedCountry == null || selectedCountry == "") {
-        alert('Lütfen Ülke Seçimi Yapınız');
-    } else {
-        console.log("Selecteds:", selectedStatus, selectedCountry);
-
-
-        getDayOneData(selectedCountry).then(data => {
-            console.log(data);
-            let chartType = "bar";
-            if (selectedStatus == "Confirmed" || selectedStatus == "Deaths" || selectedStatus == "Recovered" || selectedStatus == "Active") {
-                chartType = "line";
-            }
-            renderChart(chartType, data);
-        });
-    }
+    getDayOneData(selectedCountry).then(data => {
+        renderChart(data); // her ülke datası geldiğinde renderChart çalışır
+    });
 }
 
 
-const renderChart = function (type = "bar", covids) {
+// google chart servisini kullanarak arayüzde grafik datası gösteriyoruz.
+const renderChart = function (data) {
     let drawBackgroundColor =  function() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('number', 'X');
-        data.addColumn('number', 'Dogs');
+        var datatable = new google.visualization.DataTable();
+        datatable.addColumn('string', 'X');
+        datatable.addColumn('number', selectedStatus);
 
+        let listCovid = [];
 
-        data.addRows([
-            [0, 0], [1, 10], [2, 23], [3, 17], [4, 18], [5, 9],
-            [6, 11], [7, 27], [8, 33], [9, 40], [10, 32], [11, 35],
-            [12, 30], [13, 40], [14, 42], [15, 47], [16, 44], [17, 48],
-            [18, 52], [19, 54], [20, 42], [21, 55], [22, 56], [23, 57],
-            [24, 60], [25, 50], [26, 52], [27, 51], [28, 49], [29, 53],
-            [30, 55], [31, 60], [32, 61], [33, 59], [34, 62], [35, 65],
-            [36, 62], [37, 58], [38, 55], [39, 61], [40, 64], [41, 65],
-            [42, 63], [43, 66], [44, 67], [45, 69], [46, 69], [47, 70],
-            [48, 72], [49, 68], [50, 66], [51, 65], [52, 67], [53, 70],
-            [54, 71], [55, 72], [56, 73], [57, 75], [58, 70], [59, 68],
-            [60, 64], [61, 60], [62, 65], [63, 67], [64, 68], [65, 69],
-            [66, 70], [67, 72], [68, 75], [69, 80]
-        ]);
+        // son 60 datayı gösteriyoruz.
+        // listCovid array değişkeninin yapısına uygun hale getiriyoruz => [ ['',''],['',''],['',''], ]
+        for (var i = data.length - 60; i < data.length; i++) {
+            var diff = 0; // burda bir önceki değerdeki farkı buluyoruz.
+            switch (selectedStatus) {
+                case "Confirmed":
+                    diff = data[i].Confirmed;
+                    break;
+                case "Deaths":
+                    diff = data[i].Deaths;
+                    break;
+                case "Recovered":
+                    diff = data[i].Recovered;
+                    break;
+                case "Active":
+                    diff = data[i].Active;
+                    break;
+                case "Confirmed-Daily":
+                    diff = data[i].Confirmed - data[i - 1].Confirmed;
+                    break;
+                case "Deaths-Daily":
+                    diff = data[i].Deaths - data[i - 1].Deaths;
+                    break;
+                case "Recovered-Daily":
+                    diff = data[i].Recovered - data[i - 1].Recovered;
+                    break;
+                case "Active-Daily":
+                    diff = data[i].Active - data[i - 1].Active;
+                    break;
+                default:
+                    break;
+            }
+            diff = diff < 0 ? diff * -1 : diff;
+            var labeldate = new Date(data[i]["Date"]);
+            var parsedDate = moment(labeldate).format('DD MMMM YY');
+            listCovid.push([parsedDate, diff]); // [ ['',''],['',''],['',''], ] bu yapıya getiriyoruz
+        }
+
+        // data olarak seçilen statüyü ve tarihi grafikte gösteriyoruz.
+
+        datatable.addRows(listCovid); // [ ['',''],['',''],['',''], ] bu yapıdaki datamızı datatable a ekliyoruz.
 
         var options = {
             hAxis: {
-                title: 'Time'
+                title: 'Date',
             },
             vAxis: {
-                title: 'Popularity'
+                title: selectedCountry.toUpperCase(),
             },
             backgroundColor: '#f1f8e9'
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('chart-wrap'));
-        chart.draw(data, options);
+        var chart = new google.visualization.LineChart(chartWrap);
+        chart.draw(datatable, options);
     }
 
-    google.charts.load('current', { packages: ['corechart', 'line'] });
+    google.charts.load('current', { packages: ['corechart'] });
     google.charts.setOnLoadCallback(drawBackgroundColor);
-}
-
-const renderChart_Orjinal = function (type = "bar", data) {
-    console.log(typeof data);
-    if (typeof data != "object" || data.length == 0 || data == undefined) {
-        alert('Veriye ulaşılamadı, üzgünüz');
-    }
-
-    let ctx = document.createElement('canvas');
-    ctx.width = "400";
-    ctx.height = "400";
-
-    chartWrap.innerHTML = "";
-    chartWrap.appendChild(ctx);
-
-
-    let labels = [];
-    let datasets = [];
-
-
-    for (var i = data.length - 60; i < data.length; i++)
-    {
-        var diff = 0;
-        switch (selectedStatus) {
-            case "Confirmed":
-                diff = data[i].Confirmed;
-                break;
-            case "Deaths":
-                diff = data[i].Deaths;
-                break;
-            case "Recovered":
-                diff = data[i].Recovered;
-                break;
-            case "Active":
-                diff = data[i].Active;
-                break;
-            case "Confirmed-Daily":
-                diff = data[i].Confirmed - data[i - 1].Confirmed;
-                break;
-            case "Deaths-Daily":
-                diff = data[i].Deaths - data[i - 1].Deaths;
-                break;
-            case "Recovered-Daily":
-                diff = data[i].Recovered - data[i - 1].Recovered;
-                break;
-            case "Active-Daily":
-                diff = data[i].Active - data[i - 1].Active;
-                break;
-            default:
-                break;
-        }
-        diff = diff < 0 ? diff * -1 : diff;
-        var labeldate = new Date(data[i]["Date"]);
-        var parsedDate = moment(labeldate).format('MMMM DD YY');
-        console.log(parsedDate);
-        labels.push(parsedDate);
-        datasets.push(diff);
-    }
-
-
-    let mainChart = new Chart(ctx, {
-        type: type,
-        data: {
-            labels: labels,
-            datasets: [{
-                label: selectedCountry.toUpperCase() + " Corona Data",
-                data: datasets,
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
-
-
 }
